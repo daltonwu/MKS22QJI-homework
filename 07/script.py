fd65fc83790845e5a6d15829cd475348d2b045d3
daltonwu <dalton.z.wu@gmail.com>
2016-04-01 12:53:33 -0400

    :non-potable_water:

---
+++ 07/script.py
@@ -0,0 +1,20 @@
+def repeat(s):
+    def n_times(n):
+        return n * s
+    return n_times
+
+def r1(n):
+    return repeat('hello')(n)
+
+def r2(n):
+    return repeat('goodbye')(n)
+
+def main():
+    print '>>> r1(2)'
+    print r1(2)
+    print '>>> r2(2)'
+    print r2(2)
+    print '>>> repeat("cool")(3)'
+
+if __name__ == '__main__':
+    main()
