d65c2264e0d61bb2e5aa2f23636902e60d3ca8d9
daltonwu <dalton.z.wu@gmail.com>
2016-04-08 12:45:37 -0400

    :non-potable_water:

--- 09/apple.py
+++ 09/apple.py
@@ -1,17 +1,27 @@
+import time
+
 def foo(bar):
     return bar
 
 def fib(n):
     return n if n <= 1 else fib(n-1) + fib(n-2)
 
-@fib
 def wrapper(f):
     def inner(*arg):
         print f.func_name + str(arg)
         return f(*arg)
     return inner
 
+def timer(f):
+    def inner(*arg):
+        t0 = time.time()
+        f(*arg)
+        t1 = time.time()
+        return t1 - t0
+    return inner
+
 if __name__ == '__main__':
-    a = wrapper(fib)
-    for n in range(10):
-        print a(n)
+    fib = wrapper(fib)
+    print fib(2)
+    print
+    print timer(foo)(234)
