23ba6a621964ae6d0140545df6b0a90f31ea97dc
daltonwu <dalton.z.wu@gmail.com>
2016-04-08 12:57:48 -0400

    :non-potable_water:

--- 09/apple.py
+++ 09/apple.py
@@ -1,11 +1,5 @@
 import time
 
-def foo(bar):
-    return bar
-
-def fib(n):
-    return n if n <= 1 else fib(n-1) + fib(n-2)
-
 def wrapper(f):
     def inner(*arg):
         print f.func_name + str(arg)
@@ -20,8 +14,50 @@ def timer(f):
         return t1 - t0
     return inner
 
+@wrapper
+def fib(n):
+    return n if n <= 1 else fib(n-1) + fib(n-2)
+
+@timer
+def foo(bar):
+    return (bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar * bar *
+bar)
+
 if __name__ == '__main__':
-    fib = wrapper(fib)
     print fib(2)
     print
-    print timer(foo)(234)
+    print foo(234)
